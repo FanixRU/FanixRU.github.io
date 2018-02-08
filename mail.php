ffd333dab876256f92224a026fa5968e00acb63e
KutyaRU <36242319+KutyaRU@users.noreply.github.com>
2018-02-08 22:24:51 +0300

    ten commit

--- mail.php
+++ mail.php
@@ -1,12 +1,13 @@
 <?php
 
-$recepient = "kutya-pro@ya.ru";
-$sitename = "Название сайта";
+$recepient = "kutya-pro@yandex.ru";
+$siteName = "Кридит-Шаблон";
 
 $name = trim($_POST["name"]);
 $phone = trim($_POST["phone"]);
-$text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$message = "Имя: $name \nТелефон: $phone";
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$pagetitle = "Заявка с сайта \"$siteName\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
